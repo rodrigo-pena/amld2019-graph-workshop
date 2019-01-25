c96128cffe665e9d53e15ab4c96fe05388f3c4f3
Rodrigo Pena <rodrigo.pena@epfl.ch>
2019-01-25 12:07:29 +0100

    finish section 3 notebook

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -2,7 +2,9 @@
 
 
 import pandas as pd
+import numpy as np
 import networkx as nx
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
@@ -45,6 +47,85 @@ def assign_party_to_names(party_membership_list_path, namelist):
         print("{0} councillors couldn't be associated to a party".format(n_no_party))
     return namelist_with_parties
 
+
+def preprocess_swiss_council():
+
+    # Load information from the 50th legislature
+    adjacency = np.load('../data/council_adjacency.npy')
+    node_indices = pd.read_csv('../data/council_node_info.csv', sep=',')
+    names_with_parties = assign_party_to_names('../data/council_parties.csv', node_indices)
+
+    # Map parties to colors
+    parties_to_be_plotted = ['UDC',
+                             'PSS',
+                             'PDC',
+                             'pvl',
+                             'PLR',
+                             'PES',
+                             'PBD']
+    party_colors = ['royalblue',
+                    'r',
+                    'orange',
+                    'g',
+                    'cyan',
+                    'forestgreen',
+                    'yellow']
+    party_color_map = dict((key, value) for (key, value) in zip(parties_to_be_plotted, party_colors))
+
+    np.save('../data/council_party_color_map.npy', party_color_map)
+
+    # Assign a color to each council member, according to their parties
+    member_colors = []
+    for abbr in names_with_parties['PartyAbbreviation'].values:
+        try:
+            colorname = party_color_map[abbr]
+        except KeyError:
+            colorname = 'gray'
+        member_colors.append(colorname)
+
+    # Assemble final council dataframe
+    council_df = names_with_parties.assign(Color=pd.Series(member_colors).values)
+
+    return council_df, adjacency
+
+
+def plot_council_with_party_colors(council_df, x_coords, y_coords,
+                                   custom_colors=None):
+    # Scatter plot of the council members, colored by party affiliation
+    fig = plt.figure(figsize=(9,5))
+    ax = fig.add_subplot(111)
+
+    if custom_colors is None:
+        ax.scatter(x_coords, y_coords, c=council_df['Color'], s=50, alpha=0.8)
+        ax.set_title('2D Embedding of the Swiss National Council')
+        ax.get_xaxis().set_ticks([])
+        ax.get_yaxis().set_ticks([])
+        fig.tight_layout()
+
+        party_color_map = np.load('../data/council_party_color_map.npy').item()
+        fig = plt.figure(figsize=(9.6, 3))
+        ax = fig.add_axes([0.05, 0.15, 0.9, 0.15])
+
+        # Plot color dictionary for the party abbreviations
+        cmap = mpl.colors.ListedColormap(party_color_map.values())
+        cbar = mpl.colorbar.ColorbarBase(ax, cmap=cmap,
+                                        ticks=range(len(party_color_map.values())),
+                                        spacing='uniform',
+                                        orientation='horizontal')
+
+        cbar.ax.get_xaxis().set_ticks([])
+        for j, lab in enumerate(party_color_map.keys()):
+            cbar.ax.text((2 * j + 1) / 14.0, -.5, lab, ha='center', va='center', color='black')
+        cbar.ax.get_xaxis().labelpad = 15
+        cbar.ax.set_xlabel('Party Abbreviations')
+        cbar.ax.xaxis.set_label_coords(0.5, -1)
+
+    else:
+        ax.scatter(x_coords, y_coords, c=custom_colors, s=50, alpha=0.8)
+        ax.set_title('2D Embedding of the Swiss National Council')
+        ax.get_xaxis().set_ticks([])
+        ax.get_yaxis().set_ticks([])
+        fig.tight_layout()
 
 # For the airport data
 
